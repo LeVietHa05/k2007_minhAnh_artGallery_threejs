e025f80462acc8fd565ead2419c560da456f28a8
LeVietHa05 <ha.leviet.0504@gmail.com>
2024-08-20 16:04:13 +0700

    update control and add back wall

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { PointerLockControls } from 'three/poiterLockControls'
 
 console.log(THREE)
 
@@ -13,6 +14,7 @@ const camera = new THREE.PerspectiveCamera(
 )
 scene.add(camera)
 camera.position.z = 5 // Move the camera back 5 units
+camera.position.y = 3
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer(
@@ -42,10 +44,14 @@ scene.add(mesh)
 
 //texture loader
 let textureLoader = new THREE.TextureLoader();
-let floorTexture = textureLoader.load('img/floorTexture.jpg');
+let floorTexture = textureLoader.load('img/floorTexture3.jpg');
+floorTexture.wrapS = THREE.RepeatWrapping;
+floorTexture.wrapT = THREE.RepeatWrapping;
+floorTexture.repeat.set(10, 10)
+let wallTexture = textureLoader.load('img/white-plaster-texture.jpg');
 
 //floor plane 
-let floorPlaneGeometry = new THREE.PlaneGeometry(50, 50, 10, 10) //width, height, widthSegments, heightSegments
+let floorPlaneGeometry = new THREE.PlaneGeometry(50, 50) //width, height, widthSegments, heightSegments
 let floorPlaneMaterial = new THREE.MeshBasicMaterial({
     map: floorTexture, //texture
     side: THREE.DoubleSide//side: THREE.DoubleSide to see both sides
@@ -61,23 +67,29 @@ scene.add(wallGroup);
 //front wall
 const frontWall = new THREE.Mesh(
     new THREE.BoxGeometry(50, 20, 0.01),
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    new THREE.MeshBasicMaterial({ map: wallTexture, side: THREE.DoubleSide })
 );
 frontWall.position.z = -25;
 
 //left wall
 const leftWall = new THREE.Mesh(
     new THREE.BoxGeometry(0.01, 20, 50),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+    new THREE.MeshBasicMaterial({ map: wallTexture, side: THREE.DoubleSide })
 )
 leftWall.position.x = -25;
 //right wall
 const rightWall = new THREE.Mesh(
     new THREE.BoxGeometry(0.01, 20, 50),
-    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+    new THREE.MeshBasicMaterial({ map: wallTexture, side: THREE.DoubleSide })
 )
 rightWall.position.x = 25;
-wallGroup.add(rightWall, leftWall, frontWall);
+//back wall
+const backWall = new THREE.Mesh(
+    new THREE.BoxGeometry(50, 20, 0.01),
+    new THREE.MeshBasicMaterial({ map: wallTexture, side: THREE.DoubleSide })
+)
+backWall.position.z = 25;
+wallGroup.add(rightWall, leftWall, frontWall, backWall);
 
 //create bounding box for the walls
 for (let i = 0; i < wallGroup.children.length; i++) {
@@ -86,32 +98,146 @@ for (let i = 0; i < wallGroup.children.length; i++) {
 }
 
 //ceiling
+// wallTexture.wrapS = THREE.RepeatWrapping;
+// wallTexture.wrapT = THREE.RepeatWrapping;
+// wallTexture.repeat.set(10, 10);
 const ceiling = new THREE.Mesh(
     new THREE.BoxGeometry(50, 0.1, 50),
-    new THREE.MeshBasicMaterial({ color: 'yellow' })
+    new THREE.MeshBasicMaterial({ map: wallTexture, side: THREE.DoubleSide })
 )
 ceiling.position.y = 10;
 scene.add(ceiling);
 
+function createPainting(imageURL, width, height, position) {
+    const textureLoadder = new THREE.TextureLoader();
+    const paintingTexture = textureLoadder.load(imageURL);
+    const paintingMaterial = new THREE.MeshBasicMaterial({ map: paintingTexture });
+    const paintingGeometry = new THREE.PlaneGeometry(width, height);
+    const painting = new THREE.Mesh(paintingGeometry, paintingMaterial);
+    painting.position.set(position.x, position.y, position.z);
+    return painting;
+}
 
-//control  when press key
-document.addEventListener('keydown', onKeyDown, false);
-function onKeyDown(e) {
-    let keyCode = e.which;
-    if (keyCode == 87 || keyCode == 38) { //W key
-        camera.position.z -= 0.1;
-    } else if (keyCode == 83 || keyCode == 40) { //S key
-        camera.position.z += 0.1;
-    } else if (keyCode == 65 || keyCode == 37) { //A key
-        camera.position.x -= 0.1;
-    } else if (keyCode == 68 || keyCode == 39) { //D key
-        camera.position.x += 0.1;
+const painting1 = createPainting('artwork/1.jpg', 10, 10, new THREE.Vector3(15, 3, -24.99));
+const painting2 = createPainting('artwork/2.jpg', 10, 10, new THREE.Vector3(-15, 3, -24.99));
+
+scene.add(painting1, painting2);
+
+//control
+const controls = new PointerLockControls(camera, document.body);
+//lock the pointer (control are activated) and hide the menu
+function startExperience() {
+    //lock cursor
+    controls.lock();
+    //hide the menu
+    hideMenu();
+
+}
+
+function hideMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'none';
+}
+
+function showMenu() {
+    const menu = document.getElementById('menu');
+    menu.style.display = 'block';
+}
+
+const play_button = document.getElementById('play_button');
+play_button.addEventListener('click', startExperience);
+controls.addEventListener('lock', () => {
+    hideMenu();
+    //add event listener
+    document.addEventListener('keydown', onKeyDown, false);
+    document.addEventListener("keyup", onKeyUp, false);
+});
+controls.addEventListener('unlock', () => {
+    showMenu();
+    //remove event listener
+    document.removeEventListener('keydown', onKeyDown, false);
+    document.removeEventListener('keyup', onKeyUp, false);
+});
+
+const key = {
+    w: false,
+    s: false,
+    a: false,
+    d: false,
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+}
+
+function onKeyUp(e) {
+    if (e.key in key) {
+        key[e.key] = false;
     }
+}
+
+function onKeyDown(e) {
+    if (e.key in key) {
+        key[e.key] = true;
+    }
+}
+
+//clock to keep track of the time between frames
+const clock = new THREE.Clock();
+
+//update the movement of the camera each frame (delta is the time between frames)
+function updateMovement(delta) {
+    let moveMent = 10 * delta; //movement speed
+
+    let previousPosition = camera.position.clone();
+
+    if (key.w || key.ArrowUp) {
+        controls.moveForward(moveMent);
+    }
+    if (key.s || key.ArrowDown) {
+        controls.moveForward(-moveMent);
+    }
+    if (key.a || key.ArrowLeft) {
+        controls.moveRight(-moveMent);
+    }
+    if (key.d || key.ArrowRight) {
+        controls.moveRight(moveMent);
+    }
+
+    //check for collision
+    if (checkCollision()) {
+        //if there is a collision, reset the camera position
+        camera.position.copy(previousPosition);
+    }
+}
+
+function checkCollision() {
+    const playerBBox = new THREE.Box3(); //bounding box for the player
+    const cameraWorldPosition = new THREE.Vector3(); //camera position in world coordinates (to hold the camera position)
+
+    camera.getWorldPosition(cameraWorldPosition); //get the camera position in world coordinates (same as the player position)
+
+    //set the bounding box center to the camera position with a size of 0.5, 1.8, 0.5
+    playerBBox.setFromCenterAndSize( 
+        cameraWorldPosition,
+        new THREE.Vector3(0.5, 1.8, 0.5)
+    )
+
+    //check for collision with the walls
+    for (let i = 0; i < wallGroup.children.length; i++) {
+        const wall = wallGroup.children[i];
+        if (playerBBox.intersectsBox(wall.BBOx)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 let renderLoop = function () {
     mesh.rotation.x += 0.1 //rotate the mesh
     mesh.rotation.y += 0.1 //rotate the mesh
+    //update the movement of the camera each frame
+    updateMovement(clock.getDelta());
     //render
     renderer.render(scene, camera);
     requestAnimationFrame(renderLoop);
